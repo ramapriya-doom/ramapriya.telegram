ab0bdcc1971f35d63245fcb3ca0b4e502e5cb861
Ramapriya Doom <ramapriyakirtan@ya.ru>
2024-02-15 13:07:56 +0700

    обновление composer.json

--- lib/service/update.php
+++ lib/service/update.php
@@ -2,10 +2,20 @@
 
 namespace Ramapriya\Telegram\Service;
 
-class Update
+use Telegram\Bot\Api;
+
+final class Update
 {
+    private Api $client;
+
     public function __construct(private string $botName)
     {
+        $this->init();
+    }
+
+    private function init()
+    {
+
     }
 
     public function runHandlers(): void
