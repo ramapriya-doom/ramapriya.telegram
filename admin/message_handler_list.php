d7b0d9ae61a9893553680d25a0df0e086fcd8187
Ramapriya Doom <ramapriyakirtan@ya.ru>
2024-02-21 14:47:14 +0700

    каркас страницы со списком обработчиков входящих сообщений

--- admin/message_handler_list.php
+++ admin/message_handler_list.php
@@ -1,1 +1,17 @@
 <?php
+
+use Bitrix\Main\Loader;
+use Bitrix\Main\Localization\Loc;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin.php';
+
+/**
+ * @global CMain $APPLICATION
+ */
+
+Loc::loadMessages(__DIR__ . '/menu.php');
+Loader::includeModule('ramapriya.telegram');
+
+$APPLICATION->SetTitle(Loc::getMessage('menu_item_message_handler_list'));
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/epilog_admin.php';
