bcdbba15901275544669aad8edd96bb74f962d0d
Ramapriya Doom <ramapriyakirtan@ya.ru>
2024-02-24 15:59:16 +0700

    Страница добавления бота

--- admin/bot_edit.php
+++ admin/bot_edit.php
@@ -1,1 +1,71 @@
 <?php
+
+use Bitrix\Main\Context;
+use Bitrix\Main\Loader;
+use Bitrix\Main\Localization\Loc;
+use Ramapriya\Telegram\Entity\BotTable;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_admin.php';
+
+/**
+ * @global CMain $APPLICATION
+ */
+
+Loc::loadMessages(__FILE__);
+Loader::includeModule('ramapriya.telegram');
+
+$APPLICATION->SetTitle(Loc::getMessage('add_new_bot_title'));
+$request = Context::getCurrent()->getRequest();
+
+if ($request['save'] && check_bitrix_sessid()) {
+    BotTable::add([
+        'API_TOKEN'          => $request['API_TOKEN'],
+        'NAME'               => str_ireplace('@', '', $request['NAME']),
+        'WEBHOOK_CUSTOM_URL' => $request['WEBHOOK_CUSTOM_URL']
+    ]);
+    LocalRedirect('/bitrix/admin/telegram_bot_list.php?lang=' . LANGUAGE_ID);
+}
+
+
+$tabs = [
+    [
+        'DIV' => 'edit1',
+        'TAB' => Loc::getMessage('tab_new_bot')
+    ]
+];
+
+$tabControl = new CAdminTabControl('tabControl', $tabs);
+$tabControl->Begin();
+
+?>
+    <form action="<?= $request->getRequestUri() ?>" method="post">
+
+        <?php
+        echo bitrix_sessid_post();
+        $tabControl->BeginNextTab();
+        ?>
+
+        <tr>
+            <td width="40%"><?= Loc::getMessage('label_api_token') ?></td>
+            <td width="60%"><input type="text" required name="API_TOKEN"></td>
+        </tr>
+        <tr>
+            <td width="40%"><?= Loc::getMessage('label_name') ?></td>
+            <td width="60%"><input type="text" required name="NAME"></td>
+        </tr>
+        <tr>
+            <td width="40%"><?= Loc::getMessage('label_webhook_custom_url') ?></td>
+            <td width="60%"><input type="text" name="WEBHOOK_CUSTOM_URL"></td>
+        </tr>
+
+        <?php
+        $tabControl->EndTab();
+        $tabControl->Buttons([
+            'back_url' => '/bitrix/admin/telegram_bot_list.php?lang' . LANGUAGE_ID
+        ]);
+        ?>
+
+    </form>
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/epilog_admin.php';
---
+++ lang/ru/admin/bot_edit.php
@@ -0,0 +1,7 @@
+<?php
+
+$MESS['add_new_bot_title']        = 'Добавление нового бота';
+$MESS['tab_new_bot']              = 'Новый бот';
+$MESS['label_api_token']          = 'API-токен';
+$MESS['label_name']               = 'Username бота';
+$MESS['label_webhook_custom_url'] = 'Своя ссылка на вебхук';
